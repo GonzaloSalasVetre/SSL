1fcfef07a11d9eaa9c45ce7385cd76f901bfe2b6
GonzaloSalasVetre <gonzi.salas.vetre@gmail.com>
2023-04-20 22:16:03 -0300

    .

--- 00-CHelloworld/hello.c
+++ 00-CHelloworld/hello.c
@@ -5,7 +5,9 @@ int main()
 {
     FILE * archivo;
     archivo = fopen ("output.txt", "w");
-    fprintf(archivo, "Hola Mundo");
+    fprintf(archivo, "Hello, World!");
+
+    printf("Hello, World!");
 
     return 0;
 }
